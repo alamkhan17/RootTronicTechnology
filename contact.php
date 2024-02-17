4d02befba6b7a977abbeb7e99832692c056ed061
alamkhan17 <mohdalam165@gmail.com>
2024-02-17 22:38:17 +0530

    achievement

--- contact.php
+++ contact.php
@@ -20,7 +20,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "help.pixelcurve@gmail.com";
+        $recipient = "hr@rttechnologieslive.info";
 
         // Set the email subject.
         $subject = "$subject";
